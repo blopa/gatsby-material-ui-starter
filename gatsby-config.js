9e3823c6b351e9afb2ae54c8e0b40e175c25df22
Pablo Benmaman <montenegro.pablo@outlook.com>
2020-08-29 16:08:23 +0200

    add automatic build from github actions

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const plugins = require('./gatsby-config.plugins');
 
 module.exports = {
+    pathPrefix: 'gatsby-material-ui-starter',
     siteMetadata: {
         title: 'Gatsby Material UI Starter',
         description: 'Kick off your next, great Gatsby project with this Material UI starter.',
